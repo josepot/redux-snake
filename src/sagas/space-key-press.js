5e34de4a305759bdfe83586528ae1bdcbf8281a0
Josep M <josepot@gmail.com>
2016-07-20 00:24:36 -0400

    Improving performance of getFoodPosition (#4)

--- src/sagas/space-key-press.js
+++ src/sagas/space-key-press.js
@@ -1,8 +1,9 @@
 import R from 'ramda';
 import { select, take, put } from 'redux-saga/effects';
 import { createStructuredSelector } from 'reselect';
-import { getCurrentMoment } from '../queries/snake';
+import { List } from 'immutable';
 
+import { getCurrentMoment } from '../queries/snake';
 import { UI, game } from '../actions';
 import { GAME_STATUS, SPACE_KEY_CODE } from '../constants';
 import { initialHead } from '../config';
@@ -23,7 +24,8 @@ export function* watchSpaceKey() {
     if (gameStatus === GAME_STATUS.READY && currentMoment > 0) {
       yield put(
         game.onStartNewGame(
-          getFoodPosition([initialHead]))
+          getFoodPosition(List.of(initialHead))
+        )
       );
     }
 
