dca1933237b83d6346c27c12e494810ede897bbc
Josep M <josepot@gmail.com>
2016-07-21 23:03:24 -0400

    Improve tick frequency (#5)

--- src/sagas/tick.js
+++ src/sagas/tick.js
@@ -2,6 +2,7 @@ import R from 'ramda';
 import { delay, takeEvery } from 'redux-saga';
 import { call, select, put } from 'redux-saga/effects';
 
+import getNowInMs from '../utils/get-now-in-ms';
 import { GAME_STATUS } from '../constants';
 import { TICK_FREQUENCY } from '../config';
 import getFoodPosition from '../utils/get-food-position';
@@ -27,7 +28,10 @@ function* handleTick() {
     );
   }
 
-  yield call(delay, TICK_FREQUENCY);
+  const latestTick = yield select(R.prop('latestTick'));
+  const msToWait = TICK_FREQUENCY - (getNowInMs() - latestTick);
+
+  if (msToWait > 0) yield call(delay, msToWait);
   const gameStatus = yield select(R.prop('gameStatus'));
   if (gameStatus === GAME_STATUS.PLAYING) yield put(game.onTick());
   return null;
