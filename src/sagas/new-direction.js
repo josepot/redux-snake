c7bbac0d452967ba7f96930993ed37509abcdfbc
Josep M Sobrepere <josepot@gmail.com>
2016-07-23 17:01:14 -0400

    Improve sagas

--- src/sagas/new-direction.js
+++ src/sagas/new-direction.js
@@ -1,32 +1,30 @@
 import R from 'ramda';
-import { select, take, put } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga';
+import { select, put } from 'redux-saga/effects';
 import { createStructuredSelector } from 'reselect';
 import { getCurrentMoment } from '../queries/snake';
 
 import { UI, game } from '../actions';
 import { GAME_STATUS, KEYBOARD_DIRECTIONS } from '../constants';
 
-export function* watchNewDirection() {
-  while (true) {
-    const { payload: { keyCode } } = yield take(
-      ({ type, payload }) =>
-        type === UI.KEY_PRESSED && KEYBOARD_DIRECTIONS[payload.keyCode]
+function* handleNewDirection({ payload: { keyCode } }) {
+  const { gameStatus, currentMoment } =
+    yield select(createStructuredSelector({
+      gameStatus: R.prop('gameStatus'),
+      currentMoment: getCurrentMoment,
+    }));
+
+  if ([GAME_STATUS.PLAYING, GAME_STATUS.READY].includes(gameStatus)) {
+    yield put(
+      game.onDirectionChanged(KEYBOARD_DIRECTIONS[keyCode], currentMoment)
     );
-    const { gameStatus, currentMoment } =
-      yield select(createStructuredSelector({
-        gameStatus: R.prop('gameStatus'),
-        currentMoment: getCurrentMoment,
-      }));
-
-    if ([GAME_STATUS.PLAYING, GAME_STATUS.READY].indexOf(gameStatus) > -1) {
-      yield put(
-        game.onDirectionChanged(
-          KEYBOARD_DIRECTIONS[keyCode],
-          currentMoment
-        )
-      );
-    }
-
     if (gameStatus === GAME_STATUS.READY) yield put(game.onTick());
   }
+}
+
+export function* watchNewDirection() {
+  yield* takeEvery(({ type, payload }) =>
+    type === UI.KEY_PRESSED && KEYBOARD_DIRECTIONS[payload.keyCode],
+    handleNewDirection
+  );
 }
--- src/sagas/space-key-press.js
+++ src/sagas/space-key-press.js
@@ -1,5 +1,6 @@
 import R from 'ramda';
-import { select, take, put } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga';
+import { select, put } from 'redux-saga/effects';
 import { createStructuredSelector } from 'reselect';
 import { List } from 'immutable';
 
@@ -9,29 +10,26 @@ import { GAME_STATUS, SPACE_KEY_CODE } from '../constants';
 import { initialHead } from '../config';
 import getFoodPosition from '../utils/get-food-position';
 
-export function* watchSpaceKey() {
-  while (true) {
-    yield take(
-      ({ type, payload: { keyCode } = {} }) =>
-        type === UI.KEY_PRESSED && keyCode === SPACE_KEY_CODE
+function* handleSpaceKey() {
+  const { gameStatus, currentMoment } =
+    yield select(createStructuredSelector({
+      gameStatus: R.prop('gameStatus'),
+      currentMoment: getCurrentMoment,
+    }));
+
+  if (gameStatus === GAME_STATUS.READY && currentMoment > 0) {
+    yield put(
+      game.onStartNewGame(getFoodPosition(List.of(initialHead)))
     );
-    const { gameStatus, currentMoment } =
-      yield select(createStructuredSelector({
-        gameStatus: R.prop('gameStatus'),
-        currentMoment: getCurrentMoment,
-      }));
-
-    if (gameStatus === GAME_STATUS.READY && currentMoment > 0) {
-      yield put(
-        game.onStartNewGame(
-          getFoodPosition(List.of(initialHead))
-        )
-      );
-    }
-
-    if (gameStatus === GAME_STATUS.PLAYING) {
-      yield put(game.onTick());
-    }
+  } else if (gameStatus === GAME_STATUS.PLAYING) {
+    yield put(game.onTick());
   }
 }
 
+export function* watchSpaceKey() {
+  yield* takeEvery(
+    ({ type, payload: { keyCode } = {} }) =>
+      type === UI.KEY_PRESSED && keyCode === SPACE_KEY_CODE,
+      handleSpaceKey
+  );
+}
