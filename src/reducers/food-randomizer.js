845b064b3098efe48888fbddfd26da2ad0fe558e
Josep M Sobrepere <josepot@gmail.com>
2016-11-25 10:22:53 -0500

    Improve randomness (#18)

--- src/reducers/food-randomizer.js
+++ src/reducers/food-randomizer.js
@@ -1,9 +1,17 @@
+import R from 'ramda';
+import { fromJS } from 'immutable';
+
 import { NEW_GAME } from '../actions';
 
-const initialState = null;
+const initialState = fromJS({
+  randomSeed: 0,
+  randomIncreasser: 0,
+});
 
 export default isFoodEaten => (state = initialState, { type, payload }) => {
-  if (isFoodEaten) return state + 1;
-  if (type === NEW_GAME) return payload.randomSeed;
+  if (isFoodEaten) {
+    return state.update('randomSeed', R.add(state.get('randomIncreasser')));
+  }
+  if (type === NEW_GAME) return fromJS(payload);
   return state;
 };
